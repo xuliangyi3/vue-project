d26aeac80cb6707b0288687c243750cbf53acb58
xuliangyi3 <3520202392@qq.com>
2024-07-15 21:24:02 +0800

    Test Commit

---
+++ src/components/Base.vue
@@ -0,0 +1,37 @@
+<template>
+    <h3>模板语法</h3>
+    <p>{{ msg }}</p>
+    <p>{{ ok ? "ture" : "false" }}</p>
+    <p>{{ Hello.split("").reverse().join("") }}</p>
+    <p v-html="baidu"></p>
+    <p v-bind="attr"> {{ attr }}</p>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        msg: "Demo",
+        ok: true,
+        Hello: "dlruoW olleH",
+        baidu: "<a href='https://www.baidu.com' target='_blank'>百度</a>",
+        attr: {
+          id: "appid",
+          class: "appclass"
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .appid {
+    color: red;
+    font-size: 20rpm;
+  }
+  
+  .appclass {
+    color: blue;
+    font-size: 20rpm;
+  }
+  </style>
---
+++ src/components/V_IF.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-bind="attr" v-if="state">See</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "条件渲染",
+            attr: {
+                // id: "appID",
+                class: "appClass"
+            },
+            state:true
+        }
+    }
+}
+</script>
+<style>
+.appID{
+    color: red;
+}
+.appClass{
+    color: blue;
+}
+</style>
