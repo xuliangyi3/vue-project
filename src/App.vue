a2816d35d1d4a97ec63ba8caa71c9ca1cc077ebc
xuliangyi3 <3520202392@qq.com>
2024-07-16 15:58:05 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -7,35 +7,3 @@ import V_IF from "./components/V_IF.vue"
   <Base />
   <V_IF />
 </template>
-<<<<<<< HEAD
-=======
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
->>>>>>> 5e4166c7d1a5109733369b18e1a6c3e433b6e603
