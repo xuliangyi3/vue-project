54fd0a7a1aab1577ec9c228189e912a5359046e0
xuliangyi3 <3520202392@qq.com>
2024-07-18 15:40:45 +0800

    Style绑定

---
+++ src/components/StyleDemo.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- 对象绑定 -->
+    <p :style="{ color:activeColor,fontsize:FontSize+'px' }">Style绑定1</p>
+    <p :style="attr">Style绑定2</p>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            activeColor:'red',
+            FontSize:30,
+            attr:{
+                color:'green',
+                fontsize:'100px'
+            }
+        
+        }
+    }
+}
+</script>
+<style>
+
+</style>
