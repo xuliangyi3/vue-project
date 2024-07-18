a4bfdda8dc252033aa1dd2924ebb52fd2c309897
xuliangyi3 <3520202392@qq.com>
2024-07-18 15:09:28 +0800

    计算属性

---
+++ src/components/Calculation.vue
@@ -0,0 +1,25 @@
+<template>
+    <p>{{ msg }}</p>
+    <p>{{ calcuationFavorite }}</p>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: "计算属性",
+            attr: {
+                name: "xulaingyi",
+                age: 19,
+                favorite: ['西瓜', '香蕉', '苹果']
+            }
+        }
+    },
+    // 计算属性
+    computed: {
+        calcuationFavorite() {
+            console.log(this.attr)
+            return this.attr.favorite.length > 0 ? 'yes' : 'NO'
+        }
+    }
+}
+</script>
