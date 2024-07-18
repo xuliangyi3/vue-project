301fc5c98c2dec5d84bc86234b20b5f01b451542
xuliangyi3 <3520202392@qq.com>
2024-07-18 15:09:18 +0800

    None

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import V_IF from "./components/V_IF.vue"
 import V_BIND from "./components/V_BIND.vue"
 import V_FOR from "./components/V_FOR.vue"
 import V_ON from "./components/V_ON.vue"
+import ArrayList from "./components/ArrayList.vue"
+import Calculation from "./components/Calculation.vue"
 </script>
 
 <template>
@@ -11,5 +13,7 @@ import V_ON from "./components/V_ON.vue"
   <!-- <V_IF /> -->
   <!-- <V_BIND /> -->
   <!-- <V_FOR /> -->
-  <V_ON />
+  <!-- <V_ON /> -->
+  <!-- <ArrayList /> -->
+  <Calculation />
 </template>
