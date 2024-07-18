98e1dab155669c934742bafa71dbcd5cf8877062
xuliangyi3 <3520202392@qq.com>
2024-07-18 18:05:21 +0800

    None

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,10 @@ import V_FOR from "./components/V_FOR.vue"
 import V_ON from "./components/V_ON.vue"
 import ArrayList from "./components/ArrayList.vue"
 import Calculation from "./components/Calculation.vue"
+import ClassDemo from "./components/ClassDemo.vue"
+import StyleDemo from "./components/StyleDemo.vue"
 </script>
-
+4
 <template>
   <!-- <Base /> -->
   <!-- <V_IF /> -->
@@ -16,4 +18,7 @@ import Calculation from "./components/Calculation.vue"
   <!-- <V_ON /> -->
   <!-- <ArrayList /> -->
   <Calculation />
+  <ClassDemo />
+  <StyleDemo />
+
 </template>
