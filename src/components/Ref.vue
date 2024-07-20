de453e95ddb042accfb20322b5ed00f9ba263730
xuliangyi3 <3520202392@qq.com>
2024-07-20 20:50:32 +0800

    获取DOM

---
+++ src/components/Ref.vue
@@ -0,0 +1,28 @@
+<template>
+    <h3>模板引用</h3>
+    <h4>对DOM</h4>
+    <div ref="container" class="container">{{ msg }}</div>
+    <button @click="getElementHandle">获取DOM</button>
+</template>
+<script>
+
+/*
+DOM的操作
+    内容改变{{ 模板语法 }}
+    属性改变:v-bind:指令
+    事件:v-on
+如果没有特别需求,不要操作DOM
+*/
+export default {
+    data() {
+        return {
+            msg: '内容'
+        }
+    },
+    methods: {
+        getElementHandle() {
+            console.log(this.$refs.container.innerHTML='哈哈哈哈');
+        }
+    }
+}
+</script>
