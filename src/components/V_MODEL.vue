fedccb34fb62661dbbb77bca1542abde04d4c6f0
xuliangyi3 <3520202392@qq.com>
2024-07-20 20:50:42 +0800

    表单绑定

---
+++ src/components/V_MODEL.vue
@@ -0,0 +1,18 @@
+<template>
+    <form>
+        <input type="text"  v-model="msg">
+        <p>{{ msg }}</p>
+        <input type='checkbox' id="checkbox" v-model="checked">
+        <label for='checkbox' >{{ checked }}</label>
+    </form>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            msg:"",
+            checked:false
+        }
+    }
+}
+</script>
