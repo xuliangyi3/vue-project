b5d1f0ce1f4b0b82c636edf419f1bb1abbbf5690
xuliangyi3 <3520202392@qq.com>
2024-07-24 09:55:20 +0800

    组件传递数据（子级）

---
+++ src/components/Child.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3>Child</h3>
+    <p>{{ value }}</p>
+</template>
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    },
+    props:["value"]
+}
+
+</script>
