851a58bcfe25bab709612b30944fb604efb70e30
xuliangyi3 <3520202392@qq.com>
2024-07-24 09:55:35 +0800

    组件传递数据（父级）

---
+++ src/components/Parent.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>Parent</h3>
+    <Child :value="msg"/>
+</template>
+
+<script>
+import Child from './Child.vue';
+export default {
+    components: {
+        Child,
+    },
+    data() {
+        return {
+            msg: "组件传递数据"
+        }
+    }
+}
+</script>
